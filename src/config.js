daba9d0d630ce9c84c1faeadbc517b96ab7a0da3
Christopher <114704469+Blade-24@users.noreply.github.com>
2025-01-02 11:12:43 -0600

    add CORS support and configure allowed origins

--- src/config.js
+++ src/config.js
@@ -1,2 +1,7 @@
 
 export const PORT = 3000;
+
+export const ALLOWED_ORIGINS = [
+  "https://www.surfdata.online/",
+  "http://localhost:3000",
+]
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import express from 'express';
-import { PORT } from './config.js';
+import cors from 'cors';
+import { PORT, ALLOWED_ORIGINS } from './config.js';
 import userRoutes from "./routes/clients.routes.js";
 
 const app = express();
+
+// Configuration for CORS
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || ALLOWED_ORIGINS.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 
 app.use(express.json());
 app.use(userRoutes);
