2a16b8335147108520a8f128a78ad69443594b95
Christopher <114704469+Blade-24@users.noreply.github.com>
2025-01-03 23:50:42 -0600

    fix: update CORS configuration to include additional allowed origins

--- src/config.js
+++ src/config.js
@@ -1,7 +1,10 @@
 
 export const PORT = 3000;
 
-// export const ALLOWED_ORIGINS = [
-//   "https://surfdata.online/",
-//   "http://localhost:3000",
-// ]
+export const ALLOWED_ORIGINS = [
+  "https://surfdata.online/",
+  "https://www.surfdata.online",
+  "https://surfdata.online/login",
+  "https://www.surfdata.online/login",
+  "http://localhost:3000",
+]
--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
 import express from 'express';
-// import cors from 'cors';
-// , ALLOWED_ORIGINS
-import { PORT } from './config.js';
+import cors from 'cors';
+import { PORT, ALLOWED_ORIGINS } from './config.js';
 import userRoutes from "./routes/clients.routes.js";
 
 const app = express();
 
 // Configuration for CORS
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || ALLOWED_ORIGINS.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   }
-// }));
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Permite todas las solicitudes
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-});
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || ALLOWED_ORIGINS.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
 
 app.use(express.json());
 app.use(userRoutes);
