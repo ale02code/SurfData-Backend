ac7d1b749f02efb9c5ed60ac0dae5ac383132528
Christopher <114704469+Blade-24@users.noreply.github.com>
2025-01-03 22:32:13 -0600

    refactor: comment out CORS configuration and allowed origins in config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 export const PORT = 3000;
 
-export const ALLOWED_ORIGINS = [
-  "https://surfdata.online/",
-  "http://localhost:3000",
-]
+// export const ALLOWED_ORIGINS = [
+//   "https://surfdata.online/",
+//   "http://localhost:3000",
+// ]
--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import userRoutes from "./routes/clients.routes.js";
 const app = express();
 
 // Configuration for CORS
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || ALLOWED_ORIGINS.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || ALLOWED_ORIGINS.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// }));
 
 app.use(express.json());
 app.use(userRoutes);
