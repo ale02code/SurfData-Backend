1140fcb2014fd6edc5d1180f9f6efaf7cc964553
Christopher <114704469+Blade-24@users.noreply.github.com>
2025-01-03 23:58:44 -0600

    refactor: comment out ALLOWED_ORIGINS and CORS configuration in config and index files

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import express from 'express';
 import cors from 'cors';
-import { PORT, ALLOWED_ORIGINS } from './config.js';
+// , ALLOWED_ORIGINS 
+import { PORT } from './config.js';
 import userRoutes from "./routes/clients.routes.js";
 
 const app = express();
 
 // Configuration for CORS
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || ALLOWED_ORIGINS.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-}));
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         if (!origin || ALLOWED_ORIGINS.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     }
+// }));
+
+app.use(cors());
 
 app.use(express.json());
 app.use(userRoutes);
