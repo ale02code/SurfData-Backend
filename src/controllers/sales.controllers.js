76e1905d4811ebe9163993b88833904cb2e65226
Christopher <114704469+Blade-24@users.noreply.github.com>
2025-01-06 23:57:14 -0600

    feat: enhance getSales controller with table name validation and error handling

--- src/controllers/sales.controllers.js
+++ src/controllers/sales.controllers.js
@@ -2,11 +2,28 @@ import { pool } from "../db.js";
 
 export const getSales = async (req, res) => {
   const { name } = req.params;
-  const { rows } = await pool.query('SELECT * FROM sales.')
 
-  if (rows.length === 0) {
-    return res.status(404).json({ message: "Sale not found" });
+  // Regular expression to validate the table name
+  const avalidTableName = /^[a-zA-Z0-9_]+$/
+
+  if (!avalidTableName.test(name)) {
+    return res.status(400).json({
+      message: "Invalid table name"
+    })
   }
 
-  res.json(rows);
+  try {
+    const { rows } = await pool.query(`SELECT * FROM sales.${name}`);
+
+    if (rows.length === 0) {
+      return res.status(404).json({ message: "Sale not found" });
+    }
+
+    res.json(rows);
+  } catch (error) {
+    console.error("Error getting sales", error);
+    res.status(500).json({
+      message: "Error getting sales"
+    });
+  }
 }
