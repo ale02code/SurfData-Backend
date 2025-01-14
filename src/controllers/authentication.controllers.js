d1bf32f7c4d3db639f7e82761a6e270ca05ff300
Christopher <114704469+Blade-24@users.noreply.github.com>
2025-01-13 23:27:29 -0600

    create login controller

--- src/controllers/authentication.controllers.js
+++ src/controllers/authentication.controllers.js
@@ -1,3 +1,4 @@
+import e from "express";
 import { pool } from "../db.js";
 import bcryptjs from 'bcryptjs';
 
@@ -24,6 +25,48 @@ export const createClient = async (req, res) => {
 }
 
 export const checkingData = async (req, res) => {
-  const data = req.body;
+  const { userName, password } = req.body;
 
+  if (!userName || !password) {
+    return res.status(400).json({
+      status: "Error",
+      message: "Campos vacios. Completalos antes de reenviar el formulario"
+    });
+  }
+
+  try {
+    const { rows } = await pool.query(
+      'SELECT name, password FROM login.clients WHERE name = $1',
+      [userName]);
+
+    if (rows.length === 0) {
+      return res.status(400).json({
+        status: "Error",
+        message: "Credenciales Incorrectas"
+      });
+    }
+
+    const user = rows[0];
+    const comparePasswords = await bcryptjs.compare(
+      password, user.password
+    )
+
+    if (!comparePasswords) {
+      return res.status(400).json({
+        status: "Error",
+        message: "Credenciales Incorrectas"
+      });
+    }
+
+    res.status(200).json({
+      status: "Success",
+      message: `Usuario ${userName} autenticado.`,
+    });
+  } catch (error) {
+    console.error(`Error en la autenticación: ${e}`);
+    res.status(500).json({
+      status: "Error",
+      message: "Error en la autenticación.",
+    })
+  }
 }
