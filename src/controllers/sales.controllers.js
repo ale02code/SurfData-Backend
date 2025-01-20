ce9dbdf3bad9b83d004baf8bf89efac2c09361e7
Christopher <114704469+Blade-24@users.noreply.github.com>
2025-01-19 23:36:46 -0600

    add createSale function and route for new sales

--- src/controllers/sales.controllers.js
+++ src/controllers/sales.controllers.js
@@ -6,7 +6,6 @@ export const getSales = async (req, res) => {
     const nameClient = req.name;
 
     const nameTable = nameClient.replace(" ", "").toLowerCase();
-    console.log(nameTable);
 
     const { rows } = await pool.query(`SELECT * FROM sales.${nameTable}`);
 
@@ -17,5 +16,25 @@ export const getSales = async (req, res) => {
     res.status(200).json(rows);
   } catch (error) {
     res.status(400).json({ message: "Error getting sales" })
+  }
+}
+
+export const createSale = async (req, res) => {
+  const { producto, precio, cantidad } = req.body
+
+  try {
+    const nameClient = req.name;
+
+    const nameTable = nameClient.replace(" ", "").toLowerCase();
+
+    if (!nameClient || !producto || !precio || !cantidad) {
+      return res.status(500).json({ message: "Datos incompletos" });
+    }
+
+    const { rows } = await pool.query(`INSERT INTO sales.${nameTable} (producto, precio, cantidad) VALUES ($1, $2, $3) RETURNING *`, [producto, precio, cantidad])
+
+    res.status(200).json(rows);
+  } catch {
+    res.status(500).json({ message: "Error with create sale" });
   }
 }
--- src/routes/sales.routes.js
+++ src/routes/sales.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { getSales } from "../controllers/sales.controllers.js"
+import { getSales, createSale } from "../controllers/sales.controllers.js"
 import { verifyToken } from "../middlewares/jwt.middlewares.js";
 
 const router = Router();
 
 // getting all products
 router.get("/sales", verifyToken, getSales);
+
+router.post("/newSale", verifyToken, createSale);
 
 export default router;
