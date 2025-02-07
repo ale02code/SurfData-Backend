a83711907f020fd91f370ee2c72a357e5848189a
Christopher chacon <chchacon02@gmail.com>
2025-02-06 22:43:29 -0600

    agregar middleware de autenticación y mejorar verificación de token en jwt.middlewares

--- src/middlewares/jwt.middlewares.js
+++ src/middlewares/jwt.middlewares.js
@@ -1,23 +1,26 @@
 
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
 export const verifyToken = (req, res, next) => {
 
-  let token = req.headers.authorization;
+  let token = req.headers.authorization?.split(' ')[1];
 
   if (!token) {
     res.status(401).json({ error: "Token not provided" });
   }
 
-  token = token.split(" ")[1]
-
   try {
-    const decode = jwt.verify(token, process.env.JWT_CODE);
-    req.name = decode.name;
+    const decoded = jwt.verify(token, process.env.JWT_CODE);
+    const companyToken = codeded.company;
+    const companyUrl = req.params.companyUrl;
+
+    if (companyToken !== companyUrl) {
+      res.status(403).json({ error: "Token does not match company" });
+    }
 
     next();
   } catch (error) {
     console.log(`Error invalid token: ${e}`);
-    res.status(400).json({ Error: "Invalid token" })
+    res.status(400).json({ Error: "Access denied" })
   }
 }
